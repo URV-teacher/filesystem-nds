2f9de56fb3d2d1cb52740f0b98c3f1e7f3e7366f
AleixMT <aleixaretra@gmail.com>
2025-03-02 13:54:58 +0100

    CHANGED: filesystem example, include headers

--- source/hello-world.c
+++ source/hello-world.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 #include "hello-world.h"
 
+#include <dir.h>
+#include <unistd.h>
+
 int main(int argc, char* argv[])
 {
-	consoleDemoInit();
-	printf("Hello World\n");
+    consoleDemoInit();
+    printf("Hello World\n");
 
     while(1) {
         swiWaitForVBlank();
@@ -14,6 +17,32 @@ int main(int argc, char* argv[])
         if(pressed & KEY_START) break;
     }
 
+    /*
+     * struct stat st;
+    char filename[MAXPATHLEN]; // always guaranteed to be enough to hold a filename
+    DIR_ITER* dir;
+
+    dir = diropen ("/");
+
+    if (dir == NULL)
+    {
+        iprintf ("Unable to open the directory.\n");
+    }
+    else
+    {
+        while (dirnext(dir, filename, &st) == 0)
+        {
+            // st.st_mode & S_IFDIR indicates a directory
+            iprintf ("%s: %s\n", (st.st_mode & S_IFDIR ? " DIR" : "FILE"), filename);
+        }
+    }
+     */
     return 0;
 }
+
+
+
+
+
+
 
